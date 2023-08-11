93c0e23265908dee870b9feff0ec2e6fa89ccff8
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-08-11 21:13:00 +0300

    removed:compress - image

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
    site: 'https://unwraped.design',
-  integrations: [tailwind(), image(),  sitemap()]
+  integrations: [tailwind(),  sitemap()]
 });
