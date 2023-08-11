0282e4fb5a2be1448297a61905c67b35bdd5aefe
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-08-11 21:01:42 +0300

    removed:compress - image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import compress from "astro-compress";
-import partytown from "@astrojs/partytown";
+
+
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
    site: 'https://unwraped.design',
-  integrations: [tailwind(), image(), compress(), partytown(), sitemap()]
+  integrations: [tailwind(), image(),  sitemap()]
 });
