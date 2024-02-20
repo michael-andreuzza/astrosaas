e37102fafc84289e8cb93aefb6c8f9edb2246c9c
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-02-20 12:10:57 +0200

    cleaning up - new sections - new faq

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
+
+  
    site: 'https://lexingtonthemes.com',
   integrations: [tailwind(),  sitemap()]
 });
