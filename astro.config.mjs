ff08defb22b77cf0187b858f264b9a3f314a8756
Mher <mherarabian@gmail.com>
2025-01-29 23:44:45 -0800

    improve looks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   site: "https://productgenie.io",
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
 });
